a882c62893c7a64e4346bbbdfbaaa979641b875e
RhysTheTiger <rhyschen23@gmail.com>
2021-08-26 11:56:20 -0600

    Update GemGrid.py

--- Scripts/GameObjects/GemGrid.py
+++ Scripts/GameObjects/GemGrid.py
@@ -1,4 +1,4 @@
-import Gem
+import GameObjects.Gem
 import Objects
 import pygame
 
@@ -17,5 +17,30 @@ class GemGrid(Objects.Renderable):
 
         self.setPosition(x, y)
 
-    def addGem(self, id):
-        pass
+        self.gems = []
+        self.addGem()
+        self.addGem()
+        self.addGem()
+        self.addGem()
+        self.addGem()
+        self.addGem()
+        self.addGem()
+        self.addGem()
+
+    def addGem(self):
+        # TODO: Use id to determine which gem to add
+        gem = GameObjects.Gem.Gem()
+        gem.loadImage("../imgs/gems.png")
+        self.gems.append(gem)
+
+        length = len(self.gems)
+        row = int((length - 1) / 2)
+        column = int((length - 1) % 2)
+        vOffset = (self.rect.height - 20 - (10 * 4)) / 5
+        hOffset = (self.rect.width - 20 - (65 * 2)) / 3
+
+        x, y = self.getPosition()
+        gemX = x + 10 + (column + 1) * hOffset
+        gemY = y + 10 + (row + 1) * vOffset
+
+        gem.setPosition(gemX, gemY)
