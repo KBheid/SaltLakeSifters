30cc63f3eae783a4f1ab8fd0736e34fd30a015d6
Kayle <kbheid@live.com>
2021-08-26 11:15:49 -0600

    Added code to sifter to update image on dirt level changes

--- Scripts/GameObjects/Sifter.py
+++ Scripts/GameObjects/Sifter.py
@@ -1,5 +1,4 @@
 import pygame.display
-import Game
 import Objects
 
 
@@ -7,7 +6,11 @@ class Sifter(Objects.Renderable):
     def __init__(self):
         Objects.Renderable.__init__(self)
 
-        self.loadImage("../imgs/circle.png")
+        # Set member variables
+        self.__lastDirtCount = 0
+        self.__dirtCount = 3
+
+        self.loadImage("../imgs/tmp/sieve%d.png"%self.__dirtCount)
 
         # Set the X and Y positions to the center of the screen
         x, y = pygame.display.get_window_size()
@@ -18,3 +21,28 @@ class Sifter(Objects.Renderable):
         y -= self.rect.width / 2
 
         self.setPosition(x, y)
+
+    def addDirt(self, amount):
+        self.__lastDirtCount = self.__dirtCount
+        self.__dirtCount += amount
+        if self.__dirtCount > 3:
+            self.__dirtCount = 3
+            return
+
+        self.__updateImage()
+
+    def sift(self, amount):
+        self.__lastDirtCount = self.__dirtCount
+        self.__dirtCount -= amount
+        if self.__dirtCount < 0:
+            self.__dirtCount = 0
+            return
+
+        # TODO: Create gems
+
+        self.__updateImage()
+
+    def __updateImage(self):
+        # Load new image only if dirtCount has changed
+        if not self.__lastDirtCount == self.__dirtCount:
+            self.loadImage("../imgs/tmp/sieve%d.png" % self.__dirtCount)
