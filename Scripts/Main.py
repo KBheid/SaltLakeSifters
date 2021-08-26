a2f9d38f0c6f38098e75006626ca825c317e1789
Kayle <kbheid@live.com>
2021-08-25 23:01:03 -0600

    Removed unused code

--- Scripts/Main.py
+++ Scripts/Main.py
@@ -3,20 +3,6 @@ import Game
 import pygame.sprite
 
 
-def gameLoop(gameState):
-    if gameState.controls.spaceKeyPressed:
-        s = pygame.sprite.Sprite()
-        s.image = pygame.image.load("./imgs/jump.png").convert()
-        s.rect = s.image.get_rect()
-        gameState.renderables.append(s)
-
-
-def renderLoop(gameState, screen):
-    # TODO: For some reason, they are not rendered. Probably because I haven't looked into how sprites work at all...
-    for renderable in gameState.renderables:
-        screen.blit(renderable.image, renderable.rect)
-
-
 def main():
     game = Game.Game()
     game.mainLoop()
